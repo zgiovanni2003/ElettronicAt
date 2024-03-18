d433a7c5d28468009f95db22f297df4ed9eaa053
zgiovanni2003 <zgiovanni2003@gmail.com>
2024-03-18 09:52:38 +0100

    Password_Manager v.2.1 Aggiunta di encript con chiave passata per parametro

--- src/main/java/it/zgiovanni2003/Password_Manager.java
+++ src/main/java/it/zgiovanni2003/Password_Manager.java
@@ -3,6 +3,8 @@ package it.zgiovanni2003;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
@@ -31,6 +33,16 @@ public class Password_Manager {
         byte[] encryptedBytes = cipher.doFinal(password.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
+    
+    public static String encrypt(String password, String KEY) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encryptedBytes = cipher.doFinal(password.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+    
+    
 
     public static String decrypt(String encryptedPassword) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);

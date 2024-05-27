97a7de31f4b6eec0578d91f2730f8212af4b5cf1
zgiovanni2003 <zgiovanni2003@gmail.com>
2024-05-27 21:43:46 +0200

    Product Servlet Manage ADD - DELETE - SELECT

---
+++ src/main/java/it/zgiovanni2003/admin/AddProdotto.java
@@ -0,0 +1,85 @@
+package it.zgiovanni2003.admin;
+
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import it.zgiovanni2003.common.Database_Manager;
+
+/**
+ * Servlet implementation class AddProdotto
+ */
+
+public class AddProdotto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	String driver="com.mysql.cj.jdbc.Driver";
+    String URL_mioDB="jdbc:mysql://localhost:3306/e-commerce";
+    Database_Manager db = new Database_Manager(URL_mioDB, driver);
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddProdotto() {
+        super();
+        db.connectDriver();
+        db.connect_DB("root", "");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String csrfTokenFromRequest = request.getParameter("csrfToken");
+        String csrfTokenFromSession = CsrfTokenManager.getCsrfTokenFromSession(request);
+        System.out.println(csrfTokenFromSession);
+
+        if (csrfTokenFromRequest != null && csrfTokenFromRequest.equals(csrfTokenFromSession)) {
+        	String nome_prodotto=request.getParameter("nome_prodotto");
+        	String descrizione=request.getParameter("descrizione");
+        	String prezzo=request.getParameter("prezzo");
+        	String categoria_id=request.getParameter("categoria_id");
+        	
+        	String uploadDir =request.getServletContext().getRealPath("");
+
+            // Ottieni il file caricato
+            Part filePart = request.getPart("file");
+            String fileName = filePart.getSubmittedFileName();
+            String filePath = uploadDir + fileName;
+
+            // Controlla se il file ha un'estensione .jpg
+            if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".png")) {
+                // Salva il file sul server
+                filePart.write(filePath);
+                //response.getWriter().println("Il file è stato caricato correttamente e ha un'estensione .jsp.");
+                response.sendRedirect(request.getContextPath()+"/admin/gest-prodotti.jsp");
+            } else {
+                // Elimina il file se non ha un'estensione .jpg
+                File fileToDelete = new File(filePath);
+                if (fileToDelete.delete()) {
+                    //response.getWriter().println("Il file non è stato caricato. Si prega di caricare un file con estensione .jsp.");
+                } else {
+                    //response.getWriter().println("Errore durante l'eliminazione del file. Si prega di contattare l'amministratore di sistema.");
+                }
+                //response.getWriter().println("Il file non è stato caricato. Si prega di caricare un file con estensione .jsp.");
+            }
+        	
+        	
+    		String query = "INSERT INTO `prodotti` (`prodotto_id`, `nome_prodotto`, `descrizione`, `prezzo`, `img`, `categoria_id`) "
+    				+ "VALUES (NULL, ?, ?, ?, ?, ?)";
+
+    		String[] params= {nome_prodotto,descrizione,prezzo,filePath,categoria_id};
+
+    		db.toPost(query, params);
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Token CSRF non valido");
+        }
+	}
+
+}
---
+++ src/main/java/it/zgiovanni2003/admin/DeleteProdotti.java
@@ -0,0 +1,68 @@
+package it.zgiovanni2003.admin;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.zgiovanni2003.common.Database_Manager;
+
+/**
+ * Servlet implementation class DeleteProdotti
+ */
+@WebServlet("/DeleteProdotti")
+public class DeleteProdotti extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	String driver="com.mysql.cj.jdbc.Driver";
+    String URL_mioDB="jdbc:mysql://localhost:3306/e-commerce";
+    Database_Manager db = new Database_Manager(URL_mioDB, driver);
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteProdotti() {
+        super();
+        db.connectDriver();
+        db.connect_DB("root", "");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id=request.getParameter("id");
+		String url_img = null;
+		String query = "SELECT img FROM prodotti WHERE prodotti.prodotto_id = ?";
+		String query2 = "DELETE FROM prodotti WHERE prodotti.prodotto_id = ?";
+		String[] params= {id};
+		ResultSet result = db.toGet(query, params);
+		
+		try {
+			if(result.next()) url_img=result.getString("img");
+			File file = new File(url_img);
+			file.delete();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		int righe2=db.toPost(query2, params);
+		
+		if (righe2>0) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Errore durante l'eliminazione del prodotto");
+        }
+	}
+
+
+}
